8d94388fd1a82f74ca28129f409b2b6eb3e25bd1
Krollo54 <102640187+Krollo54@users.noreply.github.com>
2025-06-24 20:59:21 +0530

    Update synchroniser.v

--- synchroniser.v
+++ synchroniser.v
@@ -1,18 +1,23 @@
-module Top(
-    input [7:0] data_in,
-    output [7:0] data_out,
-    input rd_clk, wr_clk, rd_rst, wr_rst, rd_en, wr_en,
-    output wire empty, full
-    );
-    
-    wire [4:0] b_wrptr, g_wrptr, g_wrptr_sync;
-//    wire full, empty;
-    wire [4:0] b_rdptr, g_rdptr, g_rdptr_sync;
-    
-    Memory M1 (data_in, data_out, wr_en, wr_clk, rd_en, rd_clk, b_wrptr[3:0], b_rdptr[3:0], full, empty);
-    Read_Pointer A1 (rd_clk, rd_en, rd_rst, b_rdptr, g_rdptr, g_wrptr_sync, empty);
-    Write_Pointer A2 (wr_clk, wr_en, wr_rst, b_wrptr, g_wrptr, g_rdptr_sync, full);
-    Synchroniser S1 (rd_clk, rd_rst, g_wrptr, g_wrptr_sync);
-    Synchroniser S2 (wr_clk, wr_rst, g_rdptr, g_rdptr_sync);
+module Synchroniser(clk, rst, ptr, sync_ptr);
+
+    input clk, rst;
+    input [4:0] ptr;
+    output reg [4:0] sync_ptr;
+    reg [4:0] d;
+//    reg [5:0] d, sync_ptr; 
+   
+    always@ (posedge clk or negedge rst)
+    begin
+        if(!rst)
+        begin
+            d <= 0;
+            sync_ptr <= 0;
+        end 
+        else
+        begin
+            d <= ptr;
+            sync_ptr <= d;
+        end
+    end
     
 endmodule
