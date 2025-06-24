08a7491f55aaa698f7a92262febc8d8f47ce1b1e
Krollo54 <102640187+Krollo54@users.noreply.github.com>
2025-06-24 20:52:38 +0530

    wptr_handler.v

--- wptr_handler.v
+++ wptr_handler.v
@@ -1,16 +1,59 @@
-module write_pointer #(parameter ADDR_WIDTH = 4) (
-    input wire clk,
-    input wire rst,
-    input wire wr_en,
-    output reg [ADDR_WIDTH:0] wptr
-);
+module Write_Pointer(wr_clk, wr_en, wr_rst, b_wrptr, g_wrptr, g_rdptr_sync, full);
 
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            wptr <= 0;
-        end else if (wr_en) begin
-            wptr <= wptr + 1;
-        end
+    input wr_clk, wr_en, wr_rst;
+    output reg [4:0] b_wrptr;
+    output reg [4:0] g_wrptr;
+    input [4:0] g_rdptr_sync;
+    output reg full;
+       
+//    reg [5:0] b_wrptr, g_wrptr;
+//    reg full;
+//    assign g_wrptr = (b_wrptr >> 1) ^ b_wrptr;
+
+    
+//    assign wr_full = (g_wrptr == {~g_rdptr_sync[5], g_rdptr_sync[4:0]});
+    
+//    always@ (posedge wr_clk) begin
+
+//        if(!wr_rst) begin
+//            b_wrptr = 0;
+//        end
+//        else if(wr_en) begin
+//            b_wrptr = b_wrptr + 1;
+//        end 
+        
+//        full = wr_full;
+
+//    end
+   
+ wire [4:0] b_wrptr_next, g_wrptr_next;
+   
+  reg wrap_around;
+  wire wfull;
+  
+  assign b_wrptr_next = b_wrptr+(wr_en & !full);
+  assign g_wrptr_next = (b_wrptr_next >>1)^b_wrptr_next;
+  
+  initial begin
+    full <= 0;
+  end
+  
+  always@(posedge wr_clk or negedge wr_rst) begin
+    if(!wr_rst) begin
+      b_wrptr <= 0; // set default value
+      g_wrptr <= 0;
     end
+    else begin
+      b_wrptr <= b_wrptr_next; // incr binary write pointer
+      g_wrptr <= g_wrptr_next; // incr gray write pointer
+    end
+  end
+  
+  always@(posedge wr_clk or negedge wr_rst) begin
+    if(!wr_rst) full <= 0;
+    else        full <= wfull;
+  end
 
+  assign wfull = (g_wrptr_next == {~g_rdptr_sync[4:3], g_rdptr_sync[2:0]});
+  
 endmodule
